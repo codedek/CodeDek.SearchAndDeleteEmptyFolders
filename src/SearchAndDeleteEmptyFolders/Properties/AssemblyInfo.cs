e24fd2b5b2f31599662d4a09676e9833fb669ba7
CodeDek <codedek@gmail.com>
2019-07-03 17:17:04 -0400

    Updated: assembly Title

--- src/SearchAndDeleteEmptyFolders/Properties/AssemblyInfo.cs
+++ src/SearchAndDeleteEmptyFolders/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SearchAndDeleteEmptyFolders")]
+[assembly: AssemblyTitle("SAD Empty Folders")]
 [assembly: AssemblyDescription("Easily find and delete all empty folders from a path.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CodeDek")]
